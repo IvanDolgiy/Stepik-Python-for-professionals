16727c8cab6631619f438fea7e80207a2452b39c
IvanDolgiy <108735297+IvanDolgiy@users.noreply.github.com>
2023-03-25 15:20:20 +0100

    Update Lesson-4.4.2.16.py

--- Lesson-4.4.2.16.py
+++ Lesson-4.4.2.16.py
@@ -9,6 +9,6 @@ with open('food_services.json', encoding='utf-8') as file:
     for key, value in d.items():
         name = sorted(value, key=lambda x: x[1], reverse=True)[0]
         lst.append((key, name[0], str(name[1])))
-    lst = sorted(lst, key= lambda x: x[0])
+    lst = sorted(lst, key=lambda x: x[0])
     for item in lst:
         print(item[0]+': '+item[1]+', '+item[2])
